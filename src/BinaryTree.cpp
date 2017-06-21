71d659839fcba41bdc57cb9c0ab6e751f9637388
Sourav Mukherjee <smukherj@akamai.com>
2017-06-21 22:18:02 +0530

    ADDING THE FINAL CODE.

--- src/BinaryTree.cpp
+++ src/BinaryTree.cpp
@@ -1,15 +1,86 @@
 //============================================================================
 // Name        : BinaryTree.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Sourav Mukherjee
+// Version     : 0.3
 //============================================================================
 
 #include <iostream>
+#include<iomanip>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+struct node
+{
+  int data;
+  node* left;
+  node* right;
+};
+
+node*
+nodeCreator(int data)
+{
+  node* nodeName = new node;
+  nodeName->data = data;
+  nodeName->left = NULL;
+  nodeName->right = NULL;
+  return nodeName;
+}
+
+void
+insertNode(node* rootPtr, int data)
+{
+
+  if (rootPtr->data > data)
+    {
+      if (rootPtr->left != NULL)
+        {
+
+          insertNode(rootPtr->left, data);
+        }
+      else
+        {
+          rootPtr->left = nodeCreator(data);
+        }
+    }
+  else
+    {
+      if (rootPtr->right != NULL)
+        {
+          insertNode(rootPtr->right, data);
+        }
+      else
+        {
+          rootPtr->right = nodeCreator(data);
+        }
+    }
+
+}
+
+int
+nodePublisher(node* p, int indent = 0)
+{
+  if (p != NULL)
+    {
+      cout << p->data << "\n ";
+      if (p->left)
+        nodePublisher(p->left, indent + 4);
+      if (p->right)
+        nodePublisher(p->right, indent + 4);
+      if (indent)
+        {
+          cout << setw(indent) << '-';
+        }
+
+    }
+  return 0;
+}
+
+int
+main()
+{
+  node* rootPtr = nodeCreator(11); //Creating a root node
+  insertNode(rootPtr, 13);
+  insertNode(rootPtr, 12);
+  insertNode(rootPtr, 10);
+  nodePublisher(rootPtr);
+  return 0;
 }
